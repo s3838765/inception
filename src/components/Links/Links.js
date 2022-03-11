2820617100cc1af06cd8e3ad80debc6bd268ef84
Dib <79500311+tdib@users.noreply.github.com>
2022-03-12 02:38:33 +1100

    Fix GitHub link

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -9,7 +9,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 const Links = () => {
   return (
     <LinksContainer>
-      <Button link={'https://github.com/s3838765'} icon={<FontAwesomeIcon icon={faGithub} />} text={'GitHub'} />
+      <Button link={'https://github.com/tdib'} icon={<FontAwesomeIcon icon={faGithub} />} text={'GitHub'} />
     </LinksContainer>
   )
 }
