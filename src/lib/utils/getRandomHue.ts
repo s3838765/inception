4892a0df9ebb6e7f25e20a89f8e648da128b566a
Thomas Dib <github@tdib.xyz>
2023-05-24 00:24:13 +1000

    Add background blobs

---
+++ src/lib/utils/getRandomHue.ts
@@ -0,0 +1,4 @@
+// Set random hue and change the colour whenever the hue changes
+export function getRandomHue() {
+  return Math.floor(Math.random() * 360)
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,20 @@
   import StyledPortrait from '$lib/components/StyledPortrait.svelte'
   import Project from '$lib/components/Project.svelte'
   import Popup from '$lib/components/Popup.svelte'
+  import Blob from '$lib/components/Blob.svelte'
   import projects from '$lib/assets/projects.json'
   import { onMount } from 'svelte'
   import { Lightbulb, LightbulbOff, Github, Linkedin, ChevronUp } from 'lucide-svelte'
+  import { getRandomHue } from '$lib/utils/getRandomHue'
+  import type { PageData } from './$types'
+
+  // Load page data and set initial blob positions and visibility (to be passed into blob elements)
+  export let data: PageData
+  let blobPositions = data.randomPositions
+  let isVisible = data.isVisible
 
   // Set random hue and change the colour whenever the hue changes
-  function getRandomHue() {
-    return Math.floor(Math.random() * 360)
-  }
-  let randomHue = getRandomHue()
+  let randomHue = data.hue
   $: colour = `hsl(${randomHue}, 50%, 50%)`
 
   // Count how many times the user switches the light
@@ -60,54 +65,61 @@
     closeFn={() => popupOpen = false} />
 {/if}
 
-
-<div class='container' inert={popupOpen || null}>
-
-  <!-- Theme toggle button -->
-  <button
-    id='top'
-    class='light-dark-button'
-    title={`Turn the lights ${theme === 'dark' ? 'on' : 'off'}`}
-    on:click={toggleTheme}
-  >
-    {#if theme === 'dark'}
-      <Lightbulb size={35} />
-    {:else if theme === 'light'}
-      <LightbulbOff size={35} />
-    {/if}
-  </button>
-
-  <!-- Header information (name, links, image) -->
-  <div class='header'>
-    <h1>Thomas Dib</h1>
-
-    <div class='social-links'>
-      <a href='https://github.com/tdib' target='_blank'>
-        <Github />
-        GitHub
-      </a>
-      <a href='https://linkedin.com/in/tdib' target='_blank'>
-        <Linkedin />
-        LinkedIn
-      </a>
-    </div>
-
-    <StyledPortrait colour={colour} changeHue={() => {randomHue = getRandomHue()}} />
-  </div>
-
-  <!-- List of projects -->
-  <div class='projects'>
-    {#each projects as project, idx}
-      <Project {...project} reversed={idx % 2 === 1} theme={theme} colour={colour} />
+<div class='global-container' inert={popupOpen || null}>
+  <!-- Render all background blobs -->
+  <div class='blob-container'>
+    {#each blobPositions as blobPosition}
+      <Blob colour={colour} initialPosition={blobPosition} isVisible={isVisible}/>
     {/each}
   </div>
 
-  <a href='#top' title='Back to top' class='back-to-top' >
-    <ChevronUp />
-  </a>
-</div>
+  <div class='main-content'>
+    <!-- Theme toggle button -->
+    <button
+      id='top'
+      class='light-dark-button'
+      title={`Turn the lights ${theme === 'dark' ? 'on' : 'off'}`}
+      on:click={toggleTheme}
+    >
+      {#if theme === 'dark'}
+        <Lightbulb size={35} />
+      {:else if theme === 'light'}
+        <LightbulbOff size={35} />
+      {/if}
+    </button>
 
-<style lang='scss'>
+    <!-- Header information (name, links, image) -->
+    <div class='header'>
+      <h1>Thomas Dib</h1>
+      
+      <div class='social-links'>
+        <a href='https://github.com/tdib' target='_blank'>
+          <Github />
+          GitHub
+        </a>
+        <a href='https://linkedin.com/in/tdib' target='_blank'>
+          <Linkedin />
+          LinkedIn
+        </a>
+      </div>
+  
+      <StyledPortrait colour={colour} changeHue={() => {randomHue = getRandomHue()}} />
+    </div>
+      
+    <!-- List of projects -->
+    <div class='projects'>
+      {#each projects as project, idx}
+      <Project {...project} reversed={idx % 2 === 1} theme={theme} colour={colour} />
+      {/each}
+    </div>
+    
+    <a href='#top' title='Back to top' class='back-to-top' >
+      <ChevronUp />
+    </a>
+  </div>
+</div>
+  
+  <style lang='scss'>
   *:focus-visible {
     /* Fallback in case colour is not set for whatever reason */
     outline: 3px solid white;
@@ -135,73 +147,95 @@
     }
   }
 
-  .container {
-    backdrop-filter: blur(150px);
+
+
+  .global-container {
     position: absolute;
     left: 0;
     width: 100%;
+    overflow: hidden;
 
-    .header {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      text-align: center;
-      padding-block: 5em;
+    .blob-container {
+      z-index: -1;
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      max-width: 130em;
+      left: 50%;
+      transform: translateX(-50%);
+    }
 
-      h1 {
-        font-size: 3rem;
-        margin-block-end: 0;
-      }
+    .main-content {
+      backdrop-filter: blur(175px);
 
-      .social-links {
+      .header {
         display: flex;
-        gap: 3em;
-        margin-block: 2em;
-
-        a {
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        padding-block: 5em;
+        max-width: 100em;
+        margin: auto;
+  
+        h1 {
+          font-size: 3rem;
+          margin-block-end: 0;
+        }
+  
+        .social-links {
           display: flex;
-          gap: 1em;
-          color: var(--text);
-          background-color: var(--surface);
-          text-decoration: none;
-          padding: 1em 2em;
-          border-radius: 5em;
-          font-weight: bold;
+          gap: 3em;
+          margin-block: 3em;
+  
+          a {
+            display: flex;
+            gap: 1em;
+            color: var(--text);
+            background-color: var(--surface);
+            text-decoration: none;
+            padding: 1em 2em;
+            border-radius: 5em;
+            font-weight: bold;
+          }
         }
       }
-    }
-
-    .projects {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 7em;
-      padding: 5em;
-      max-width: 100em;
-      margin: auto;
-    }
-
-    .back-to-top {
-      color: var(--text);
-      background-color: var(--surface);
-      border-radius: 1em 1em 0em 0em;
-      display: flex;
-      width: 5em;
-      justify-content: center;
-      margin-inline: auto;
+  
+      .projects {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 10em;
+        padding: 5em;
+        max-width: 100em;
+        margin: auto;
+      }
+  
+      .back-to-top {
+        color: var(--text);
+        background-color: var(--surface);
+        border-radius: 1em 1em 0em 0em;
+        display: flex;
+        width: 5em;
+        justify-content: center;
+        margin-inline: auto;
+      }
     }
   }
 
 
   @media (max-width: 500px) {
-    .container {
-      padding: 2em;
-      padding-block-end: 0;
+    .global-container {
+      .main-content {
+        backdrop-filter: blur(75px);
+        .header .social-links {
+          flex-direction: column;
+          gap: 1em;
+          margin-block-start: 1em;
+        }
 
-      .header .social-links {
-        flex-direction: column;
-        gap: 1em;
-        margin-block-start: 1em;
+        .projects {
+          padding: 3em;
+        }
       }
     }
 
